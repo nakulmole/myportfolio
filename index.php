2192c807a6807190a0e277504284669aed9924d1
Nakul Mole <nakulmole7272@gmail.com>
2021-02-23 14:31:44 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="description" content="Portfolio Website">
+    <meta name="keywords" content="Nakul,Mole,Portfolio,Information,CV,Skills,Experience,Interest,Contact,HireM,Me">
+    <meta name="author" content="Nakul Mole">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+   
+    <title>My Portfolio</title>
+    <link rel="stylesheet" href="my_style.css">
+
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>    
+</head>
+
+<body>
+
+<!-- Header -->
+<nav class="navbar">
+    <div class="max-width">
+    <div class="logo"><a href="#">Portfo<span>lio</span>.</a> 
+    </div>
+    <ul class="menu">
+        <li><a href="#home" class="menu-btn">Home</a></li>
+        <li><a href="#about" class="menu-btn">About</a></li>
+        <li><a href="#education" class="menu-btn">Education</a></li>
+        <li><a href="#skills" class="menu-btn">Skills</a></li>
+        <li><a href="#contact" class="menu-btn">Contact</a></li>
+    </ul>
+
+    <div class="menu-btn">
+        <i class="fas fa-bars"></i>
+    </div>
+
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+</div>
+
+</nav>
+
+<!-- Home Section -->
+
+
+<section class="home" id="home">
+
+    <div class="max-width">
+
+
+
+
+        <div class="home-content">
+
+      
+            <div class="text-1">Hello, my name is</div>
+            <div class="text-2">Nakul Mole</div>
+            <div class="text-3">And I'm a <span  class="typing"></span></div>
+            <a href="#contact">Contact Me</a>
+            <ul class="dots">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul> 
+         
+            
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+
+<!-- About Section -->
+
+<section class="about" id="about">
+    <div class="max-width">
+        <h2 class="title">About me</h2>
+        <div class="about-content">
+
+            <div class="column left">
+                <img src="images/profile-1.jpeg" alt="image"/>
+            </div>
+
+            <div class="column right">
+                 <div class="text">I'm Nakul and I'm a <span class="typing-2"> </span></div>
+                 <p> 
+                    Graduate of Computer Engineering with experiences of working across Android Development, Website Development, Prototyping, Designing etc.
+                    I've built some innovative project my self and I'm looking for a role where I can grow and learn from other experienced team members. 
+
+
+                </p>
+                 <a href="#">Download CV</a>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+<hr>
+
+<!-- Education section start -->
+
+
+<section class="education" id="education">
+    <div class="max-width">
+        <h2 class="title">Education</h2>
+        <div class="edu-content">
+
+            <div class="timeline-item" date-is='2019'>
+                <h1>Dr. D.Y. Patil Institute Of Technology, Pimpri,Pune</h1>
+                <p>
+                    B.E. in Computer Engineering
+                </p>
+                <p>SGPA 7.6</p>
+            </div>
+            
+            <div class="timeline-item" date-is='2016'>
+                <h1>Sri Shivaji Polytechnic Institute, Parbhani.</h1>
+                <p>
+                    Diploma In Computer Engineering
+                </p>
+                <p>74%</p>
+            </div>
+            
+            <div class="timeline-item" date-is='2013'>
+                <h1>Katruwar Kabra Mantri(K.K.M.) College, Manwath, Parbhani</h1>
+                <p>
+                    HSC
+                </p>
+                <p>
+                    48%
+                </p>
+            </div>
+
+            <div class="timeline-item" date-is='2009'>
+                <h1>Netaji Subhash Vidhyalaya, Manwath, Parbhani</h1>
+                <p>
+                    SSC
+                </p>
+                <p>
+                    58%
+                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<hr>
+
+<!-- Skills section start -->
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">My creative skills & experiences.</div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, ratione error est recusandae consequatur, iusto illum deleniti quidem impedit, quos quaerat quis minima sequi. Cupiditate recusandae laudantium esse, harum animi aspernatur quisquam et delectus ipsum quam alias quaerat? Quasi hic quidem illum. Ad delectus natus aut hic explicabo minus quod.</p>
+                    <a href="#">Read more</a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>PHP</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MySQL</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <hr>
+ <!-- contact section start -->
+
+ <!-- contact section start -->
+ <section class="contact" id="contact">
+    <div class="max-width">
+        <h2 class="title">Contact me</h2>
+        <div class="contact-content">
+            <div class="column left">
+                <div class="text">Get in Touch</div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos harum corporis fuga corrupti. Doloribus quis soluta nesciunt veritatis vitae nobis?</p>
+                <div class="icons">
+                    <div class="row">
+                        <i class="fas fa-user"></i>
+                        <div class="info">
+                            <div class="head">Name</div>
+                            <div class="sub-title">Nakul Mole</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <i class="fas fa-map-marker-alt"></i>
+                        <div class="info">
+                            <div class="head">Address</div>
+                            <div class="sub-title">Pune, Maharashtra, India</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <i class="fas fa-envelope"></i>
+                        <div class="info">
+                            <div class="head">Email</div>
+                            <div class="sub-title">nakulmole7272@gmail.com</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="column right">
+               
+
+          
+                <div class="text">Message me</div>
+                <form action="process.php" method="post">
+                    <div class="fields">
+                        <div class="field name">
+                            <input name="Uname" type="text" placeholder="Name" required>
+                        </div>
+                        <div class="field email">
+                            <input name="Email" type="email" placeholder="Email" required>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <input type="text" name="Subject" placeholder="Subject" required>
+                    </div>
+                    <div class="field textarea">
+                        <textarea cols="30" name="msg" rows="10" placeholder="Message.." required></textarea>
+                    </div>
+                    <div class="button">
+                        <button  name="btn-send" type="submit">Send message</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<footer>
+    <span>Created By <a href="#">Nakul Mole</a> | <span class="far fa-copyright"></span> 2021 All rights reserved.</span>
+</footer>
+
+<script src="script.js"></script>
+
+</body>
+
+
+
+
+
+</html>
